3f931da40d75b79b94630e25234c33a9048d2e9c
Alexandre <alextopota528@gmail.com>
2020-06-05 15:41:19 +0200

    dep

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/tpv-angular'));
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/tpv-angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/tpv-angular/src/index.html'));
 });
 
 // default Heroku PORT
