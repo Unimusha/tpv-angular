56e1b2c314cf9dddcf2df861ef3f6be8cb86c113
Alexandre <alextopota528@gmail.com>
2020-06-05 15:29:46 +0200

    Desplegandooo

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-
-
-
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
+// Serve static files....
 app.use(express.static(__dirname + '/dist/tpv-angular'));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ng-blog/tpv-angular'));
+
+// Send all requests to index.html
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/tpv-angular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
