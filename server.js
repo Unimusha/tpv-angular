229c1f7e6c0f851d31af013716e2a7127fb56ee8
Alexandre <alextopota528@gmail.com>
2020-06-07 14:43:17 +0200

    depleoy nº 21

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/tpv-angular'));
+app.use(express.static('./dist/tpvAngular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/tpv-angular/' }),
+    res.sendFile('index.html', { root: 'dist/tpvAngular/' }),
 );
 
 app.listen(process.env.PORT || 8080);
